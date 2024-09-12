8435a4e9a48c933d976c801f13972620c54d9e83
justy <justy@DonLish>
2024-09-12 13:43:07 -0400

    another  method

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -4,8 +4,14 @@ public class DemoTest {
 	
 	public static void addNumbers(int a, int b) {
 		
-		System.out.println("Numbers are: "a+b);
+		System.out.println("Numbers are: " a+ b );
+
 		
+	}
+	
+	public void homeAdress(String name) {
+		
+		System.out.println("My address is: " + name);
 	}
 
 }
